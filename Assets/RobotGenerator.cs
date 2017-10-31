a590507051dc37388593940ebae16f6a51c1aefd
Italo Lelis <italohdc@gmail.com>
2017-10-31 09:43:48 -0200

    Added New Terrain model

---
+++ Assets/RobotGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject myPrefab;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
